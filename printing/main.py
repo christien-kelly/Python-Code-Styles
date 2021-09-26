1ffb1de0aaf3445eca951a90bcf584d14269ef62
Christien Kelly <christien@ridewithshare.com>
2021-09-26 19:39:17 -0400

    final print guide.

--- printing/main.py
+++ printing/main.py
@@ -7,10 +7,52 @@ def main():
     """executes the print module"""
 
     items: List[int] = [1, 2, 3, 4, 5]
-    
-    for item in items:
-        print(item)
+    items_string: list = ["1", "2", "3"]
+
+    values: tuple[Any] = ("Christien", "Python", 6)
+
+    # 0. 
+    # print(items)
+
+    # print(f"{items}")
+
+    # for item in items:
+    #     print(item)
+
+    # 1. separator (* == destructuring list.)
+    # print(*items, sep= ", ")
+
+    # 2. join
+    # print("Is a valid number \n".join(map(str, items)))
+
+    # 3. Special characters
+    print("Employee %s \nWorks as a %s Developer \nAnd is %d feet tall." % values)
+
+    # %d number
+    # %s string
+    # %g general number
+    # %f float
+
 
     
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
